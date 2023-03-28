e3fa4d6973b6169507bd2702563c017136559797
Min <74136649+Kim-min-gi@users.noreply.github.com>
2023-03-29 04:34:34 +0900

    [level 0] Title: 7의 개수, Time: 8.99 ms, Memory: 68.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
@@ -9,6 +9,15 @@ class Solution {
         for(int j =0; j<s.length(); j++){
             if(s.charAt(j) == '7') answer++;
         }
+        
+        
+        
+        // for(int i =0; i<array.length; i++){
+        //    String str = String.valueOf(array[i]);
+        //    for(int j =0; j<str.length(); j++){
+        //        if(str.charAt(j)=='7') answer++;
+        //    }
+        // }
         return answer;
     }
 }
