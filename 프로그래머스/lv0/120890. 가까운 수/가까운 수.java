f3bd8cdc0d5aaf5657364230a26c3b64b9a2c5d4
Min <74136649+Kim-min-gi@users.noreply.github.com>
2023-04-01 14:29:15 +0900

    [level 0] Title: 가까운 수, Time: 0.45 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.java
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.java
@@ -1,20 +1,14 @@
 import java.util.*;
 class Solution {
     public int solution(int[] array, int n) {
-           int min=100;
-        for(int i=0; i<array.length; i++) {
-                if(Math.abs(array[i]-n)<min) {            //array의 i번째 정수와 n의 차가 min보다 작으면
-                    min=Math.abs(array[i]-n);             //min 에 저장
-                }
+        int answer = 0;
+        Arrays.sort(array);
+        for(int i = 1 ; i < array.length ; i++){
+            if(Math.abs(n-array[0]) > Math.abs(n-array[i])){
+                array[0] = array[i];
             }
-        ArrayList<Integer> arr=new ArrayList<Integer>();  //가까운 수 최대 2개를 저장할 list   
-            for(int i=0; i<array.length; i++) {
-                if((Math.abs(array[i]-n))==(min)) {       //정수n과의 차가 min 이면
-                    arr.add(array[i]);                    //list에 저장
-                }
-            }
-        Collections.sort(arr);                            //오름차순으로 나열
-        
-        return arr.get(0);
+        }
+        answer = array[0];
+        return answer;
     }
 }
