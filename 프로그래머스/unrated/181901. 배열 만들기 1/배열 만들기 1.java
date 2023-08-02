69c0d4071e50a36fd64395f6390b857f1c5c785c
Min <74136649+Kim-min-gi@users.noreply.github.com>
2023-08-02 10:13:21 +0900

    [unrated] Title: 배열 만들기 1, Time: 0.03 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int n, int k) {
+        int length = n/k;
+        int[] answer = new int[length];
+        int index = 0;
+        for(int i =k; i<=n; i+=k){
+            answer[index] = i;
+            index++;
+        }
+        return answer;
+    }
+}
