7c5634d87c619d475153b22c13f29cbee07524b6
Min <74136649+Kim-min-gi@users.noreply.github.com>
2023-08-12 23:13:37 +0900

    [unrated] Title: 1로 만들기, Time: 0.02 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.java
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.java
@@ -1,16 +1,22 @@
 class Solution {
     public int solution(int[] num_list) {
     	int answer = 0;
-        for (int i : num_list) {
-            while(i != 1) {
-                if (i % 2 == 0) i /= 2;
-                else {
-                    i -= 1;
-                    i /= 2;
-                }
-                answer++;                
+        for (int i : num_list) {      
+            
+            while(i != 1){
+                i /= 2;
+                answer++;
             }
-        }
+            
+            // while(i > 1) {
+            //     if (i % 2 == 0) i /= 2;
+            //     else {
+            //         i -= 1;
+            //         i /= 2;
+            //     }
+            //     answer++;                
+            // }
+            }
         return answer;
-        }
+       }
 }
