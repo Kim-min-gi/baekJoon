6cdcc8d16e95dc57f9869c206bd34093e9d5b629
Min <74136649+Kim-min-gi@users.noreply.github.com>
2023-08-22 13:33:58 +0900

    [unrated] Title: 세로 읽기, Time: 8.69 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.java
@@ -1,18 +1,23 @@
 class Solution {
     public String solution(String my_string, int m, int c) {
         String answer = "";
-        int length = my_string.length() / m;
-        char[][] cArray = new char[length][m];
-        int index = 0;
-        for(int i =0; i<length; i++){
-            for(int j=0; j<m; j++){
-                cArray[i][j] = my_string.charAt(index);
-                index++;
-            }
-        }
+//         int length = my_string.length() / m;
+//         char[][] cArray = new char[length][m];
+//         int index = 0;
+//         for(int i =0; i<length; i++){
+//             for(int j=0; j<m; j++){
+//                 cArray[i][j] = my_string.charAt(index);
+//                 index++;
+//             }
+//         }
         
-        for(int i =0; i<length; i++){
-            answer += cArray[i][c-1];
+//         for(int i =0; i<length; i++){
+//             answer += cArray[i][c-1];
+//         }
+        
+        
+        for(int i = c-1 ; i<my_string.length(); i+=m){
+            answer += my_string.charAt(i);
         }
         
         return answer;
