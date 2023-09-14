e212a532ed23d27c907a485e399cd9146bf04c12
Min <74136649+Kim-min-gi@users.noreply.github.com>
2023-09-15 07:35:55 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 1.73 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String phone_number) {
+        String answer = "";
+        int length = phone_number.length() - 4;
+        String number = phone_number.substring(length);
+        for(int i=0; i<length; i++){
+            answer += '*';
+        }
+        
+        return answer + number;
+    }
+}
