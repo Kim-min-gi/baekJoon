2ad1a6a9153accd4dc7a27caacd2e103c1ea9fad
Min <74136649+Kim-min-gi@users.noreply.github.com>
2023-09-18 03:15:47 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.02 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = false;
+        
+        if(s.length() == 4 || s.length() == 6){
+            
+            try{
+                
+                int a = Integer.parseInt(s);
+                answer = true;
+                
+            }catch(NumberFormatException e){
+                
+            }
+            
+        }
+        
+        return answer;
+    }
+}
