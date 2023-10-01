b35d8132534defa5d037cfc4970918b7342766de
Min <74136649+Kim-min-gi@users.noreply.github.com>
2023-10-02 03:09:52 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 1.29 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/README.md
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/README.md
@@ -1,10 +1,10 @@
 # [unrated] 푸드 파이트 대회 - 134240 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/134240?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/134240) 
 
 ### 성능 요약
 
-메모리: 76.8 MB, 시간: 1.50 ms
+메모리: 77.7 MB, 시간: 1.29 ms
 
 ### 구분
 
--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -15,7 +15,13 @@ class Solution {
         builder.reverse();
         reverseBuilder += builder.toString();
        
-        
+        // StringBuilder builder = new StringBuilder();
+        // for (int i=1; i<food.length; i++) {
+        //     int result = food[i] / 2;
+        //     builder.append(String.valueOf(i).repeat(result));
+        // }
+        // String answer = builder + "0";
+        // return answer + builder.reverse();
         
         return reverseBuilder;
     }
