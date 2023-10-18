f245d365e688d96cf7caefc36ab58aebaeec6498
Min <74136649+Kim-min-gi@users.noreply.github.com>
2023-10-18 16:06:39 +0900

    [level 1] Title: 콜라 문제, Time: 0.02 ms, Memory: 66.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        int temp = 0;
+        
+        while(true){
+            
+            if(n < a) break;
+            
+            temp = (n / a) * b;
+            n = n - ((n / a) * a) + temp;
+            answer += temp;
+        }
+        
+        return answer;
+    }
+}
