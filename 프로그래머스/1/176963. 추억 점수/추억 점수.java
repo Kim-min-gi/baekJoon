bd5aee990dfec8855cdc5e586920413cdd43c6b5
Min <74136649+Kim-min-gi@users.noreply.github.com>
2023-10-19 21:49:11 +0900

    [level 1] Title: 추억 점수, Time: 0.07 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -1,24 +1,6 @@
-import java.util.*;
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
-        int[] answer = new int[photo.length];
-        
-        HashMap<String,Integer> map = new LinkedHashMap<>();
-        
-        for(int i=0; i< name.length; i++){
-            map.put(name[i], yearning[i]);
-        }
-        
-        for(int i=0; i<photo.length; i++){
-            int result = 0;
-            for(int j=0; j<photo[i].length; j++){
-                if(map.containsKey(photo[i][j])){
-                    result += map.get(photo[i][j]);
-                }
-            }
-            answer[i] = result;
-        }
-             
+        int[] answer = {};
         return answer;
     }
 }
