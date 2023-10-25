3f8f55f236ac6fab9b8f9229284ee3cfd423a341
Min <74136649+Kim-min-gi@users.noreply.github.com>
2023-10-26 01:29:12 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 7.25 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -2,10 +2,10 @@ import java.util.*;
 class Solution {
     public String solution(String s) {
         String[] list = s.split(" ");
-        int max = -99999;
-        int min = 99999;
+        int max , min;
+        max = min = Integer.parseInt(list[0]);
         
-        for(int i=0; i<list.length; i++){
+        for(int i=1; i<list.length; i++){
           max = Math.max(max,Integer.parseInt(list[i]));
           min = Math.min(min,Integer.parseInt(list[i]));
         }
