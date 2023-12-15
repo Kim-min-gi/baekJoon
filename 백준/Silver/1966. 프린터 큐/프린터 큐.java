2e771dc3a497392a40b3860ffc396adcca96bfa2
Min <74136649+Kim-min-gi@users.noreply.github.com>
2023-12-15 20:50:12 +0900

    [Silver III] Title: 프린터 큐, Time: 152 ms, Memory: 15072 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -1,50 +1,60 @@
-import java.io.*;
-import java.util.*;
- 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-		
-        int tCase = Integer.parseInt(br.readLine()); // 테스트케이스 개수
-	
-        for (int i=0; i<tCase; i++) {
+        int testCase = Integer.parseInt(br.readLine());
+
+        for (int i =0; i<testCase; i++){
             st = new StringTokenizer(br.readLine());
-			
-            int N = Integer.parseInt(st.nextToken()); // 문서의 개수
+
+            int N = Integer.parseInt(st.nextToken());
             int M = Integer.parseInt(st.nextToken());
-			
-            Queue<int[]> q = new LinkedList<int[]>();
-			
-            st = new StringTokenizer(br.readLine());
-			
-            for (int j=0; j<N; j++) {
+
+            Queue<int[]> que = new LinkedList<>();
+
+            st  = new StringTokenizer(br.readLine());
+
+
+            for (int j = 0; j<N; j++){
                 int num = Integer.parseInt(st.nextToken());
-				
-                q.add(new int[] {j, num});
+
+                que.add(new int[] {j,num});
             }
-			
+
             int cnt = 0;
-            while (true) {
-                int[] cur = q.poll();
-                boolean chk = true; // 중요도가 높은 것이 있는지 여부
-				
-                for (int[] curQ : q) {
-                    if (curQ[1] > cur[1]) {
+
+            while (true){
+                int[] arr = que.poll();
+                boolean chk = true;
+
+                for (int[] a : que){
+                    if (a[1] > arr[1]){
                         chk = false;
                         break;
                     }
                 }
-                
-                if (chk) {
+
+                if (chk){
                     cnt++;
-                    if (cur[0] == M) break;
-                } else {
-                    q.add(cur);
+                    if (arr[0] == M) break;
+                }else{
+                    que.add(arr);
                 }
+
             }
-			
+
             System.out.println(cnt);
+
         }
+
+
     }
 }
