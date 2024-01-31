2720a97db8f6aab2fda290c60152ad28a9634cfe
Min <74136649+Kim-min-gi@users.noreply.github.com>
2024-02-01 03:01:55 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 124 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -1,48 +1,43 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.StringTokenizer;
- 
-public class Main {
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
- 
-		int T = Integer.parseInt(br.readLine());	 
- 
-		while (T -- > 0) {
-			
-			HashMap<String, Integer> hm = new HashMap<>();	
-			
-			int N = Integer.parseInt(br.readLine());	
-			
-			while (N-- > 0) {
-				StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-				
-				st.nextToken();	
-				
-				String kind = st.nextToken();	
-				
-				if (hm.containsKey(kind)) {
-					hm.put(kind, hm.get(kind) + 1);
-				} 
-				else {
-					hm.put(kind, 1);
-				}
-			}
- 
-			int result = 1;
- 
-			for (int val : hm.values()) {
-				result *= (val + 1);
-			}
-			
-			sb.append(result - 1).append('\n');
- 
-		}
-		System.out.println(sb);
-	}
- 
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class Main{
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        for (int i =0; i<N; i++){
+
+            int M = Integer.parseInt(br.readLine());
+            Map<String, Integer> map = new HashMap<>();
+
+            for (int j=0; j<M; j++){
+                st = new StringTokenizer(br.readLine());
+                st.nextToken();
+
+                String key = st.nextToken();
+
+                map.put(key, map.getOrDefault(key,0) + 1);
+
+            }
+
+            int result = 1;
+
+            for (int num : map.values()){
+                result *= (num + 1); // 안입은 경우도 생각해야 해서 +1
+            }
+
+            //알몸인 상태는 제거해야 하므로 -1
+            sb.append(result - 1).append('\n');
+
+        }
+
+        System.out.println(sb);
+
+    }
 }
