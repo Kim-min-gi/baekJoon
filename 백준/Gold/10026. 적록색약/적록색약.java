7165110212407798194cd1512077b1504c64d9b4
Min <74136649+Kim-min-gi@users.noreply.github.com>
2024-02-11 23:07:00 +0900

    [Gold V] Title: 적록색약, Time: 136 ms, Memory: 15284 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,76 +1,107 @@
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.io.*;
 
-public class Main {
-	//그림 좌표 관련 클래스
-    static class position{
-        int x,y;
-        public position(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
+class Node{
+    int x,y;
+    public Node(int x,int y){
+        this.x = x;
+        this.y = y;
     }
+
+}
+
+
+public class Main{
+
+    static char[][] arr;
+    static boolean[][] visited;
+
     static int N;
-    static char[][] art;	
-    static boolean[][] visited;	
-    static int[] dx = {0,0,-1,1};	
-    static int[] dy = {-1,1,0,0};	
-    static public void  main(String[] args) throws IOException {
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
+
+
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         N = Integer.parseInt(br.readLine());
-        art = new char[N][N];
-        for(int i=0;i<N;i++){
-            String str = br.readLine();
-            for(int j=0;j<N;j++)
-                art[i][j] = str.charAt(j);
-        }
-        bw.write(search() + " ");	
-        colorChange();		
-        bw.write(search() + "\n");	
-        bw.flush();		//결과 출력
-        bw.close();
-        br.close();
-    }
-    static int search(){
+        arr = new char[N][N];
         visited = new boolean[N][N];
+
+        for (int i =0; i<N; i++){
+            String s = br.readLine();
+
+            for (int j =0; j<N; j++){
+                arr[i][j] = s.charAt(j);
+            }
+        }
+
         int count = 0;
-        for(int i=0;i<N;i++){
-            for(int j=0;j<N;j++){
-                if(!visited[i][j]){
-                    bfs(j,i,art[i][j]);
+        for (int i =0; i<N; i++){
+            for (int j = 0; j<N; j++){
+                if (!visited[i][j]){
+                    bfs(i,j,arr[i][j]);
                     count++;
                 }
             }
         }
-        return count;
-    }
-    static void bfs(int x, int y, char color){
-        Queue<position> queue = new LinkedList<>();
-        visited[y][x] = true;
-        queue.add(new position(x,y));
-        while(!queue.isEmpty()){
-            position cur = queue.poll();
-            for(int i=0;i<4;i++){
-                int tempX = cur.x + dx[i];
-                int tempY = cur.y + dy[i];
-                if(tempX>=0 && tempY>=0 && tempX<N && tempY<N && !visited[tempY][tempX]){
-                    if(art[tempY][tempX] == color){
-                        visited[tempY][tempX] = true;
-                        queue.add(new position(tempX,tempY));
-                    }
+
+        sb.append(count).append(" ");
+
+
+        for (int i = 0; i<N; i++){
+            for (int j = 0; j<N; j++){
+                if (arr[i][j] == 'R'){
+                    arr[i][j] = 'G';
                 }
             }
         }
 
-    }
-    static void colorChange() {
-        for (int i = 0; i < N; i++) {
-            for(int j=0;j<N;j++){
-                if(art[i][j] == 'R')
-                    art[i][j] = 'G';
+        visited = new boolean[N][N];
+        count = 0;
+
+        for (int i =0; i<N; i++){
+            for (int j = 0; j<N; j++){
+                if (!visited[i][j]){
+                    bfs(i,j,arr[i][j]);
+                    count++;
+                }
             }
         }
+
+        sb.append(count);
+
+        System.out.println(sb);
+
     }
+
+    static void bfs(int x, int y,char color){
+        Queue<Node> que = new LinkedList<>();
+        que.add(new Node(x,y));
+        visited[x][y] = true;
+
+        while (!que.isEmpty()){
+           Node now = que.poll();
+           for (int i =0; i<4; i++){
+               int next_x = now.x + dx[i];
+               int next_y = now.y + dy[i];
+
+               if (next_x >= 0 && next_y >= 0 && next_x < N && next_y < N && !visited[next_x][next_y]){
+                   if (arr[next_x][next_y] == color){
+                       visited[next_x][next_y] = true;
+                       que.add(new Node(next_x,next_y));
+                   }
+               }
+
+
+           }
+
+
+        }
+
+
+    }
+
 }
