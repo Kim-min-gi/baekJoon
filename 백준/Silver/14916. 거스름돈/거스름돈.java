5bd3d4177c782914bd9f19a08f7945ee95de5442
Min <74136649+Kim-min-gi@users.noreply.github.com>
2024-02-14 16:19:40 +0900

    [Silver V] Title: 거스름돈, Time: 124 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class Main{
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int count = 0;
+
+        while (true){
+            if (n%5 == 0){
+                count += n / 5;
+                System.out.println(count);
+                break;
+            }else{
+                n -= 2;
+                count++;
+            }
+
+            if (n<0){
+                System.out.println(-1);
+                break;
+            }
+        }
+
+       
+
+    }
+
+}
