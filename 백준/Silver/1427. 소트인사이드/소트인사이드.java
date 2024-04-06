df15b8b8b6340c152693d7cccc84ea44860369c4
Min <74136649+Kim-min-gi@users.noreply.github.com>
2024-04-06 17:27:11 +0900

    [Silver V] Title: 소트인사이드, Time: 128 ms, Memory: 14440 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,41 +1,21 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-
-
+import java.io.IOException;
+import java.util.Arrays;
+ 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] stringArr = br.readLine().split("");
-        int[] arr = new int[stringArr.length];
-
-        int index = 0;
-        for (String c : stringArr){
-            arr[index++] = Integer.parseInt(c);
-        }
-
-        for (int i = 0; i<arr.length-1; i++){
-            int minIndex = i;
-            for (int j = i+1; j<arr.length; j++){
-                if (arr[minIndex] < arr[j]) minIndex = j;
-            }
-
-            int temp = arr[minIndex];
-            arr[minIndex] = arr[i];
-            arr[i] = temp;
-        }
-
-        String answer = "";
-        for (int num : arr){
-            answer += num;
-        }
-
-
-        System.out.println(answer);
-    }
-
-
-
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		char[] arr = br.readLine().toCharArray();
+ 
+ 
+		Arrays.sort(arr);
+ 
+		for (int i = arr.length - 1; i >= 0; i--) {
+			System.out.print(arr[i]);
+		}
+ 
+	}
 }
