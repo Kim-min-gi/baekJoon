18eff0bfa89b33ed99dc09943df96d2bb48da7b6
Min <74136649+Kim-min-gi@users.noreply.github.com>
2024-11-09 20:28:52 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 8.13 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -2,13 +2,8 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] seoul) {       
-        int index = 0;
-        
-        for (String s : seoul){
-            if (!s.equals("Kim")) index++;
-            else break;
-        }
-        
-        return "김서방은 "+index+"에 있다";
+       int index = Arrays.asList(seoul).indexOf("Kim");
+
+        return "김서방은 "+ index + "에 있다";
     }
 }
