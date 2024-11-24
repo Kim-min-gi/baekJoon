3d6d0dcbbcc632a31698fbf37e098f625751fd67
Min <74136649+Kim-min-gi@users.noreply.github.com>
2024-11-24 17:04:41 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.39 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,14 +1,13 @@
 class Solution {
     public int solution(String s) {
-        String replaceS = s;
-        String[] sArr = {"zero","one","two","three","four"
-            ,"five","six","seven","eight","nine"};
         
-        
-        for(int i = 0; i<sArr.length; i++){
-           replaceS = replaceS.replaceAll(sArr[i],String.valueOf(i)); 
+        String[] words = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+
+
+        for (int i = 0; i<words.length; i++){
+            s = s.replaceAll(words[i], String.valueOf(i));
         }
         
-        return Integer.parseInt(replaceS);
+        return Integer.parseInt(s);
     }
 }
