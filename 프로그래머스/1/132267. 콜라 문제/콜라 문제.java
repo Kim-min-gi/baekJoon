1a9bb39c0e7c0ea938a9b543a7c2c420b98284de
Min <74136649+Kim-min-gi@users.noreply.github.com>
2024-11-25 14:42:03 +0900

    [level 1] Title: 콜라 문제, Time: 0.09 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -1,14 +1,12 @@
 class Solution {
     public int solution(int a, int b, int n) {
         int answer = 0;
-        int temp = 0;
         
-        while(true){
-            if(n < a) break;
-            
-            temp = (n / a) * b;
-            n = n % a + temp;
-            answer += temp;
+        while (n >= a){
+            answer += (n / a) * b;
+
+            n = (n / a) * b  + (n % a);
+
         }
         
         return answer;
