e7ed13a48c8c0f4e02b02433972ab5584beacbf3
Min <74136649+Kim-min-gi@users.noreply.github.com>
2024-12-10 16:16:13 +0900

    [level 2] Title: 피보나치 수, Time: 1.69 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,20 +1,16 @@
 class Solution {
     public int solution(int n) {
-        int answer = fibonacci(n);
-        //System.out.println(answer);
-        return answer;
-    }
-    
-    public int fibonacci(int n) {
-        int[] f = new int[n+1];
+
+        int prev = 0;
+        int curr = 1;
         
-        f[0] = 0;
-        f[1] = 1;
+        for(int i =2; i<=n; i++){
+            int next = (prev + curr) % 1234567;
+            prev = curr;
+            curr = next;
+        }        
         
-        for (int i=2; i<= n; i++){
-            f[i] = (f[i-1] + f[i-2]) % 1234567;
-        }
-        
-        return f[n];
+
+        return curr;
     }
 }
