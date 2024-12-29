168cb3501c7e11684900cb264b3db8fc81e75f04
DANGHYEON <rlaalsrl3023@naver.com>
2024-12-29 21:42:51 +0900

    인프런 문제 연속 자연수의 합

---
+++ 인프런/배열/연속된 자연수의 합.java
@@ -0,0 +1,67 @@
+
+
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+
+
+
+    public static int getAnswer(int n,int[] arr) {
+        int sum = 0;
+        int count = 0;
+        int lt = 0;
+
+        for(int i= 0; i<arr.length; i++){
+            sum += arr[i];
+            if (sum == n) count++;
+
+            while (sum >= n){
+                sum -= arr[lt++];
+                if (sum == n) count++;
+            }
+        }
+
+        return count;
+    }
+
+
+
+
+
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = n/2+1;
+        int[] arr = new int[m];
+
+
+        for (int i =0; i<m; i++){
+            arr[i] = i+1;
+        }
+
+
+//        int[] arr = new int[n];
+//
+//        st = new StringTokenizer(br.readLine());
+//        int index = 0;
+//        while (st.hasMoreTokens()){
+//            arr[index++] = Integer.parseInt(st.nextToken());
+//        }
+
+        System.out.println(getAnswer(n,arr));
+
+
+    }
+}
