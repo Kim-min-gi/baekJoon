f9ee0bcd9da4a1aa77f3371a4aaf239d56761058
Min <74136649+Kim-min-gi@users.noreply.github.com>
2025-01-19 20:24:48 +0900

    [level 2] Title: 카펫, Time: 0.08 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,28 +1,25 @@
-
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        int size = brown + yellow;
-        
-        for(int i =3; i<size; i++){
-            
-            int num = size / i;
-            
-            if(size % i == 0 && num >= 3){
-                int col = Math.max(i,num);
-                int row = Math.min(i,num);
-                
-                int center = (col-2) * (row-2);
-                
-                if(center == yellow){
-                    answer[0] = col;
-                    answer[1] = row;
-                    return answer;
+ 
+        int carpet = brown + yellow;
+ 
+        for (int i = 3; i <= carpet; i++) {
+            int col = i; 
+            int row = carpet / col; 
+ 
+            if (row < 3) {
+                continue;
+            }
+ 
+            if (row >= col) {
+                if ((row - 2) * (col - 2) == yellow) {
+                    answer[0] = row;
+                    answer[1] = col;
+                    break;
                 }
             }
         }
-        
-        
         return answer;
     }
 }
