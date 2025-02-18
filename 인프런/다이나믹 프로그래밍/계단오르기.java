f8fdd7275eb88e836c106c1b1e9fd639102cfe89
DANGHYEON <rlaalsrl3023@naver.com>
2025-02-18 18:44:54 +0900

    인프런 문제 계단 오르기

---
+++ 인프런/다이나믹 프로그래밍/계단오르기.java
@@ -0,0 +1,38 @@
+
+
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+
+public class Main {
+
+    static int[] arr;
+
+    public static int getAnswer(int num){
+        arr[0] = 1;
+        arr[1] = 1;
+
+        for (int i=2; i<=num; i++){
+            arr[i] = arr[i-2] + arr[i-1];
+        }
+
+
+        return arr[num];
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        arr = new int[num+1];
+
+        System.out.println(getAnswer(num));
+
+
+    }
+}
