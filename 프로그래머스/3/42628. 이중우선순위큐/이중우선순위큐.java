4b9a9a15b6a32137c1d571126e37b6da78bade92
Min <74136649+Kim-min-gi@users.noreply.github.com>
2025-04-09 23:57:31 +0900

    [level 3] Title: 이중우선순위큐, Time: 104.95 ms, Memory: 148 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,26 +1,37 @@
 import java.util.*;
 class Solution {
     public int[] solution(String[] operations) {
-        int[] answer = new int[2];
-        PriorityQueue<Integer> maxQueue = 
-            new PriorityQueue(Collections.reverseOrder());
-        PriorityQueue<Integer> minQueue = new PriorityQueue();
-        
-        for(String operation : operations){
-            String[] ops = operation.split(" ");
-            int num = Integer.parseInt(ops[1]);
-            if(ops[0].equals("I")){
-                maxQueue.add(num); minQueue.add(num);
-            }else{
-                if(num == -1)
-                    maxQueue.remove(minQueue.poll());
-                else 
-                    minQueue.remove(maxQueue.poll());
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (String operation : operations) {
+            String[] parts = operation.split(" ");
+            String command = parts[0];
+            int value = Integer.parseInt(parts[1]);
+
+            if (command.equals("I")) {
+                minHeap.offer(value);
+                maxHeap.offer(value);
+            } else if (command.equals("D")) {
+                if (value == 1) {
+                    if (!maxHeap.isEmpty()) {
+                        int max = maxHeap.poll();
+                        minHeap.remove(max);
+                    }
+                } else if (value == -1) {
+                    if (!minHeap.isEmpty()) {
+                        int min = minHeap.poll();
+                        maxHeap.remove(min);
+                    }
+                }
             }
         }
+
+        if (minHeap.isEmpty()) {
+            return new int[] {0, 0};
+        } else {
+            return new int[] {maxHeap.peek(), minHeap.peek()};
+        }
         
-        answer[0] = maxQueue.isEmpty() ? 0 : maxQueue.peek();
-        answer[1] = minQueue.isEmpty() ? 0 : minQueue.peek();
-        return answer;
     }
 }
