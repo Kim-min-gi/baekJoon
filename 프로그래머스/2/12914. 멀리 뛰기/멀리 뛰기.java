63fdffc3f0a01961d2f3e4ec6df679cb1f8895ee
Min <74136649+Kim-min-gi@users.noreply.github.com>
2025-08-23 00:53:00 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.15 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,13 +1,15 @@
 class Solution {
     public long solution(int n) {
-        long[] dp = new long[n + 2];
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 2;
+        if(n <= 2) return n;
         
-        for (int i = 3; i <= n ; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2])%1234567;
+        long[] dp = new long[n + 1];
+        dp[1] = 1L;
+        dp[2] = 2L;
+        
+        for(int i =3; i <= n; i++){
+            dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
         }
+        
         return dp[n];
     }
 }
